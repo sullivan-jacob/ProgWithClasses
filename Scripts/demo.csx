dcb392274bce4ec6254b65d1cc3c7a02c7af545d
Jacob Sullivan <sul23002@byui.edu>
2024-05-02 20:08:06 -0600

    Completed the Prep1 and Prep2 assignments.

---
+++ Scripts/demo.csx
@@ -0,0 +1,8 @@
+// using Internal;
+using System;
+
+Console.WriteLine("Hello, what is your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}!");
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? .");
+        int grade = int.Parse(Console.ReadLine());
+        int gradeOnes = grade%10;
+        int gradeTens = (grade - gradeOnes)/10;
+        string gradeModifier = "";
+        if (gradeOnes >= 7) {
+            gradeModifier = "+";
+        } else if (gradeOnes <= 3) {
+            gradeModifier = "-";
+        }
+        string letterGrade = "";
+        bool passed = true;
+        if (gradeTens >= 9) {
+            if (gradeModifier == "-") {
+                letterGrade = "A-";
+            } else {
+                letterGrade = "A";
+            }
+        } else if (gradeTens == 8) {
+            letterGrade = $"B{gradeModifier}";
+        } else if (gradeTens == 7) {
+            letterGrade = $"C{gradeModifier}";
+        } else if (gradeTens == 6) {
+            letterGrade = $"D{gradeModifier}";
+            passed = false;
+        } else if (gradeTens <= 5) {
+            letterGrade = "F";
+            passed = false;
+        }
+        Console.WriteLine($"You currently have a {letterGrade} in this class.");
+        if (passed) {
+            Console.WriteLine("You are currently passing this class, good job!");
+        } else {
+            Console.WriteLine("You are currently failing this class. Work harder!");
+        }
+
     }
 }
